a27f2d9ca8d61fa27a21eab8fc2b52e56380ed6f
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-08-06 21:27:17 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,16 +10,26 @@ export default function Home() {
         Bhavesh Patil
       </h1>
       <p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground">
-  Hey there! I'm Bhavesh Patil, the web wizard from Chatrapati Sambhajinagar, currently battling the dragons of a BSC in IT at MGM&apos;s Dr. G. Y. Pathrikar. Based in India, I brew web applications like a caffeinated barista. As an introvert, I thrive in my code cave, enjoying the silent symphonies of coding, music, and horror stories.
-</p>
-
-<p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground">
-  🛍️ <Link href="https://thepixelstore.vercel.app" target="_blank">ThePixelStore</Link>: An app store inspired by the iOS App Store. (React, Framework7)<br />
-  🎥 <Link href="https://darkvalor.vercel.app" target="_blank">DarkValor</Link>: A YouTube client that&rsquo;s super minimal. (HTML, Tailwind CSS, JavaScript)<br />
-  🔍 <Link href="https://stackx.vercel.app" target="_blank">StackX</Link>: Google&rsquo;s search engine, my way. (HTML, CSS, JavaScript)<br />
-  📖 <Link href="https://forwordin.vercel.app" target="_blank">ForWord</Link>: A dictionary app. (Ionic React)<br />
-  🌾 <Link href="https://sanjaypatil.vercel.app" target="_blank">Sanjay Patil&apos;s Website</Link>: My dad&apos;s website. (Information about fertilizers)
-</p>
+        Hey there! I&apos;m Bhavesh Patil, the web wizard from Chatrapati Sambhajinagar, currently battling the dragons of a BSC in IT at MGM&apos;s Dr. G. Y. Pathrikar. Based in India, I brew web applications like a caffeinated barista. As an introvert, I thrive in my code cave, enjoying the silent symphonies of coding, music, and horror stories.
+      </p>
+      <p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground">
+        <strong>Skills & Technologies</strong>
+      </p>
+      <p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground">
+        <p align="center">
+          <Image src="https://skillicons.dev/icons?i=html,css,js,bootstrap,tailwindcss,c,md,react,next,ts&theme=light&perline=5" alt="Languages" height="300" width="300" />
+        </p>
+      </p>
+      <p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground">
+        <strong>Projects & Experience</strong>
+      </p>
+      <p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground">
+        🛍️ <Link href="https://thepixelstore.vercel.app" target="_blank">ThePixelStore</Link>: An app store inspired by the iOS App Store. (React, Framework7)<br />
+        🎥 <Link href="https://darkvalor.vercel.app" target="_blank">DarkValor</Link>: A YouTube client that&apos;s super minimal. (HTML, Tailwind CSS, JavaScript)<br />
+        🔍 <Link href="https://stackx.vercel.app" target="_blank">StackX</Link>: Google&apos;s search engine, my way. (HTML, CSS, JavaScript)<br />
+        📖 <Link href="https://forwordin.vercel.app" target="_blank">ForWord</Link>: A dictionary app. (Ionic React)<br />
+        🌾 <Link href="https://sanjaypatil.vercel.app" target="_blank">Sanjay Patil&apos;s Website</Link>: My dad&apos;s website. (Information about fertilizers)
+      </p>
       <p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground">
         <strong>GitHub Stats & Badges</strong><br />
         Check out my GitHub bling:
@@ -41,7 +51,7 @@ export default function Home() {
       </p>
       <p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground">
         <strong>Education</strong><br />
-        BSC in IT from MGM's Dr. G. Y. Pathrikar, Chatrapati Sambhajinagar<br />
+        BSC in IT from MGM&apos;s Dr. G. Y. Pathrikar, Chatrapati Sambhajinagar<br />
         SSC from MIT-VGS, Chatrapati Sambhajinagar<br />
         HSC from DES Highschool, Dharangaon, Malkapur, Buldhana
       </p>
