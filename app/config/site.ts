02fe709b17131f330d02cb6b71619838b9454ba9
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2025-02-16 00:00:12 +0530

    Create site.ts

---
+++ app/config/site.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  name: 'Bhavesh Patil',
+  url: 'https://iambhvsh.vercel.app',
+  description: 'Frontend Developer focused on building innovative and modern web applications.',
+  author: 'Bhavesh Patil',
+  twitter: '@iambhvsh',
+  github: 'iambhvsh',
+}
